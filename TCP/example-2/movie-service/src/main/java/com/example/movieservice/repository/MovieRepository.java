4a2264c6a4313f30c8ec661d8369b5bea1a9c896
ksereda <kirill.sereda@gmail.com>
2020-02-26 10:36:02 +0300

    added ReactiveMongoRepository in repository

--- TCP/example-2/movie-service/src/main/java/com/example/movieservice/repository/MovieRepository.java
+++ TCP/example-2/movie-service/src/main/java/com/example/movieservice/repository/MovieRepository.java
@@ -1,9 +1,10 @@
 package com.example.movieservice.repository;
 
 import com.example.movieservice.model.Movie;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRepository extends ReactiveCrudRepository<Movie, String> {
+public interface MovieRepository extends ReactiveMongoRepository<Movie, String> {
 }
